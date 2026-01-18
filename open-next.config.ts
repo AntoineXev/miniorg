b9d26109e9987f7603c5fbd23decf0c2aec3161f
Antoine Hervet <antoine@aher.vet>
2026-01-18 01:30:02 +0100

    Regenerate prisma on build

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,9 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
-	incrementalCache: r2IncrementalCache,
+	// Pas de cache incrémental R2 - utilise le cache par défaut (mémoire)
+	// Si vous avez besoin de cache persistant plus tard, vous pouvez:
+	// 1. Créer un bucket R2 et utiliser r2IncrementalCache
+	// 2. Utiliser Workers KV avec kvIncrementalCache
 });
